5c2a76c73f53294f02d1f9d7785684f2c039827d
TF-1031 <toddefarrell@gmail.com>
2025-08-31 19:02:51 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,77 @@
+window.BORGIA_TEAMS = [
+  {
+    "name": "JV Volleyball",
+    "ics": "http://www2.arbitersports.com/iCal/Team/schedule.ics?id=zUGKWfMAUMDi5ICcRJfPRQ%3d%3d",
+    "schedule": ""
+  },
+  {
+    "name": "Varsity Volleyball",
+    "ics": "http://www2.arbitersports.com/iCal/Team/schedule.ics?id=cq2PfPo%2b%2bmxs6eI2BSHJWQ%3d%3d",
+    "schedule": ""
+  },
+  {
+    "name": "Freshman Volleyball",
+    "ics": "http://www2.arbitersports.com/iCal/Team/schedule.ics?id=LGmylvwW1RLyVXU8PJv0FA%3d%3d",
+    "schedule": ""
+  },
+  {
+    "name": "JV Softball",
+    "ics": "http://www2.arbitersports.com/iCal/Team/schedule.ics?id=JCzueDFVhaz6OqLdBtaBgg%3d%3d",
+    "schedule": ""
+  },
+  {
+    "name": "Varsity Softball",
+    "ics": "http://www2.arbitersports.com/iCal/Team/schedule.ics?id=Ue2CoqdpoIJA2am%2bc7x5nw%3d%3d",
+    "schedule": ""
+  },
+  {
+    "name": "JV Soccer – JV",
+    "ics": "http://www2.arbitersports.com/iCal/Team/schedule.ics?id=T8q7gRNMDMmQfq0CVwmLbg%3d%3d",
+    "schedule": ""
+  },
+  {
+    "name": "JV Soccer",
+    "ics": "http://www2.arbitersports.com/iCal/Team/schedule.ics?id=BoB1kHmmu%2fkumYRuQu1xMQ%3d%3d",
+    "schedule": ""
+  },
+  {
+    "name": "Varsity Soccer – V",
+    "ics": "http://www2.arbitersports.com/iCal/Team/schedule.ics?id=X3ZZWuazdrxLlL24FwSC%2bA%3d%3d",
+    "schedule": ""
+  },
+  {
+    "name": "Freshman Football",
+    "ics": "http://www2.arbitersports.com/iCal/Team/schedule.ics?id=9xiAEXnWVWx08jXer9zZuQ%3d%3d",
+    "schedule": ""
+  },
+  {
+    "name": "JV Football",
+    "ics": "http://www2.arbitersports.com/iCal/Team/schedule.ics?id=tRMiEFda927m3MM3MQBEjQ%3d%3d",
+    "schedule": ""
+  },
+  {
+    "name": "Varsity Football",
+    "ics": "http://www2.arbitersports.com/iCal/Team/schedule.ics?id=o3hRcWm%2boO%2bv54qae0%2f9QA%3d%3d",
+    "schedule": ""
+  },
+  {
+    "name": "Varsity Cross Country",
+    "ics": "http://www2.arbitersports.com/iCal/Team/schedule.ics?id=SrPIiZejlpI8oP43aI55sA%3d%3d",
+    "schedule": ""
+  },
+  {
+    "name": "Varsity Competitive Cheer",
+    "ics": "http://www2.arbitersports.com/iCal/Team/schedule.ics?id=BjNdM42b4hFs2JXTTCY4qA%3d%3d",
+    "schedule": ""
+  },
+  {
+    "name": "Varsity Dance",
+    "ics": "http://www2.arbitersports.com/iCal/Team/schedule.ics?id=mcylqcYIBxb1XfowG1vnQA%3d%3d",
+    "schedule": ""
+  },
+  {
+    "name": "Varsity Marching Band",
+    "ics": "http://www2.arbitersports.com/iCal/Team/schedule.ics?id=6TAkALSrtvtHgjTTNZP5Ng%3d%3d",
+    "schedule": ""
+  }
+];
